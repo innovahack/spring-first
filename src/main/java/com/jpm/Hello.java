c192d8d0c0cb7d8c31a146e8ca36b849513244b5
innovahack <vijitk@hotmail.com>
2021-10-03 18:28:23 +0800

    New Beans

---
+++ src/main/java/com/jpm/Hello.java
@@ -0,0 +1,13 @@
+package com.jpm;
+
+public class Hello {
+    private String greet;
+
+    public String getGreet() {
+        return greet;
+    }
+
+    public void setGreet(String greet) {
+        this.greet = greet;
+    }
+}
---
+++ src/main/java/com/jpm/MyNumber.java
@@ -0,0 +1,14 @@
+package com.jpm;
+
+public class MyNumber {
+
+    private int testNumber;
+
+    public int getTestNumber() {
+        return testNumber;
+    }
+
+    public void setTestNumber(int testNumber) {
+        this.testNumber = testNumber;
+    }
+}
