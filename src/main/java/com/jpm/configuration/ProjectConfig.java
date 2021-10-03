52fe9d7b6022faa8d8beae354f84ae60b27fc4c6
innovahack <vijitk@hotmail.com>
2021-10-03 18:30:24 +0800

    added new Beans method and provided named Beans

--- src/main/java/com/jpm/configuration/ProjectConfig.java
+++ src/main/java/com/jpm/configuration/ProjectConfig.java
@@ -1,5 +1,7 @@
 package com.jpm.configuration;
 
+import com.jpm.Hello;
+import com.jpm.MyNumber;
 import com.jpm.Parrot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +9,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProjectConfig {
 
-    @Bean
-    Parrot parrot(){
+    @Bean(name="parrot1")
+    Parrot parrot1(){
         var parrot = new Parrot();
         parrot.setName("First Parrot");
         return parrot;
+    }
+
+    @Bean(name="parrot2")
+    Parrot parrot(){
+        var parrot = new Parrot();
+        parrot.setName("Second Parrot");
+        return parrot;
+    }
+
+    @Bean(name="3rd-parrot")
+    Parrot parrot3(){
+        var parrot = new Parrot();
+        parrot.setName("Third Parrot");
+        return parrot;
+    }
+
+
+    @Bean
+    String hello(){
+        var greet = new Hello();
+        greet.setGreet("Hello Spring");
+        return greet.getGreet();
+    }
+
+    @Bean
+    Integer myNumber(){
+        var num = new MyNumber();
+        num.setTestNumber(104);
+        return num.getTestNumber();
     }
 }
